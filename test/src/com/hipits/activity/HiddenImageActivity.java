1965da554ec35c881be32fd6565bec20000a34df
junhwan <dkwmzkqks01@naver.com>
2013-01-26 10:31:42 +0900

    퀴즈 UI 추가

--- test/src/com/hipits/activity/HiddenImageActivity.java
+++ test/src/com/hipits/activity/HiddenImageActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
--- test/src/com/hipits/activity/QuizActivity.java
+++ test/src/com/hipits/activity/QuizActivity.java
@@ -3,7 +3,6 @@ package com.hipits.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 
 import com.hipits.R;
 
@@ -12,12 +11,18 @@ public class QuizActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
-
-		findViewById(R.id.homeImageViewButton).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				finish();
-			}
-		});
+		
 	}
+	
+	public void onClick(View view){
+		switch (view.getId()) {
+		case R.id.homeImageViewButton:
+			finish();
+			break;
+			
+		default:
+			break;
+		}
+	}
+
 }
